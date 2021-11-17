17f8cbe7e10967203bb83ca442b80bcdf7105c0d
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-16 23:15:38 -0500

    arrows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentA1Component } from './module-a/component-a1/component-a1.component';
-import { ComponentA2Component } from './module-a/component-a2/component-a2.component';
-import { ComponentB1Component } from './module-b/component-b1/component-b1.component';
-import { ComponentB2Component } from './module-b/component-b2/component-b2.component';
+import { ComponentA1Component } from './module-a/component-a1/component-a1.component';  //<=
+import { ComponentA2Component } from './module-a/component-a2/component-a2.component';  //<=
+import { ComponentB1Component } from './module-b/component-b1/component-b1.component';  //<=
+import { ComponentB2Component } from './module-b/component-b2/component-b2.component';  //<=
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentA1Component, 
-    ComponentA2Component,
-    ComponentB1Component, 
-    ComponentB2Component
+    ComponentA1Component,   //<=
+    ComponentA2Component,   //<=
+    ComponentB1Component,   //<=
+    ComponentB2Component    //<=
   ],
   imports: [
     BrowserModule,
