f996e082411048cd0a431350c00713d0459bdb47
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-16 22:51:39 -0500

    Included components B

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentA1Component } from './module-a/component-a1/component-a1.component';
 import { ComponentA2Component } from './module-a/component-a2/component-a2.component';
+import { ComponentB1Component } from './module-b/component-b1/component-b1.component';
+import { ComponentB2Component } from './module-b/component-b2/component-b2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentA1Component, 
-    ComponentA2Component
+    ComponentA2Component,
+    ComponentB1Component, 
+    ComponentB2Component
   ],
   imports: [
     BrowserModule,
