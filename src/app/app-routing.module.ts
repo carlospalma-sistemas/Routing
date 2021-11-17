c87ddce741199c9eb57ca6c134df7f2d6d56fef9
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-16 23:27:55 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentA1Component } from './module-a/component-a1/component-a1.component';
+import { ComponentA2Component } from './module-a/component-a2/component-a2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", redirectTo:"ruta1", pathMatch:"full" },
+  { path:"ruta1", component:ComponentA1Component },
+  { path:"ruta2", component:ComponentA2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
