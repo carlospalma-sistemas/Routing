fdbca33b7eb7395cc32b24ba84499865fb8cd832
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-16 23:08:51 -0500

    arrows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentA1Component } from './module-a/component-a1/component-a1.component';
-import { ComponentA2Component } from './module-a/component-a2/component-a2.component';
+import { ComponentA1Component } from './module-a/component-a1/component-a1.component'; //<=
+import { ComponentA2Component } from './module-a/component-a2/component-a2.component'; //<=
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentA1Component, 
-    ComponentA2Component
+    ComponentA1Component,  //<=
+    ComponentA2Component   //<=
   ],
   imports: [
     BrowserModule,
--- src/app/module-a/module-a.module.ts
+++ src/app/module-a/module-a.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentA1Component } from './component-a1/component-a1.component';
-import { ComponentA2Component } from './component-a2/component-a2.component';
-import { ComponentA3Component } from './component-a3/component-a3.component';
+import { ComponentA1Component } from './component-a1/component-a1.component';  //<=
+import { ComponentA2Component } from './component-a2/component-a2.component';  //<=
+import { ComponentA3Component } from './component-a3/component-a3.component';  //<=
 
 
 
 @NgModule({
   declarations: [
-    ComponentA1Component,
-    ComponentA2Component,
-    ComponentA3Component
+    ComponentA1Component,   //<=
+    ComponentA2Component,   //<=
+    ComponentA3Component    //<=
   ],
   imports: [
     CommonModule
